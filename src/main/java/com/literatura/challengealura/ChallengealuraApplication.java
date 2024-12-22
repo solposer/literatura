fb2dc76c298fb7cdeba87bd2d16994dd10be98b2
marisol quevedo <marisolfqd@gmail.com>
2024-12-22 18:26:57 -0500

    lolplease

---
+++ src/main/java/com/literatura/challengealura/ChallengealuraApplication.java
@@ -0,0 +1,13 @@
+package com.literatura.challengealura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengealuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengealuraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/literatura/challengealura/ChallengealuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.literatura.challengealura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengealuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
