e4c3bb4fbd2890662208252593ab0b60039f5ea3
marisol quevedo <marisolfqd@gmail.com>
2024-12-22 18:56:24 -0500

    creando alura application

--- src/main/java/com/literatura/challengealura/ChallengealuraApplication.java
+++ src/main/java/com/literatura/challengealura/ChallengealuraApplication.java
@@ -1,13 +1,22 @@
 package com.literatura.challengealura;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
-public class ChallengealuraApplication {
+public class ChallengealuraApplication implements CommandLineRunner {
+	@Autowired
+	private BookRepository repository;
+	public static void main(String[] args){
+		SpringApplication.run(ChallengealuraApplication.class,args);
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChallengealuraApplication.class, args);
 	}
 
+	@Override
+	public void run (String... args) throws Exception {
+		Principal principal = new Principal(repository);
+		principal.muestraElMenu();
+	}
 }
