2e0cb13591a3711e11c4dcca852dae442cfecfca
marisol quevedo <marisolfqd@gmail.com>
2024-12-22 23:43:06 -0500

    datosbooks record

---
+++ src/main/java/com/literatura/challengealura/model/DatosBooks.java
@@ -0,0 +1,11 @@
+package com.literatura.challengealura.model;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DatosBooks (
+    @JsonAlias("title") String titulo,
+    @JsonAlias("authors")String autores,
+    @JsonAlias("languages")String lenguajes ){
+    }
+}
