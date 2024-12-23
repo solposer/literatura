5acf07deb65b313ba68e9150e6e45c9406e1cc97
marisol quevedo <marisolfqd@gmail.com>
2024-12-22 20:24:31 -0500

    convierte datos creando

---
+++ src/main/java/com/literatura/challengealura/service/ConvierteDatos.java
@@ -0,0 +1,17 @@
+package com.literatura.challengealura.service;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.literatura.challengealura.model.DatosBooks;
+
+public class ConvierteDatos implements IConvierteDatos{
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public <T> T obtenerDatos(String json, Class<T> clase) {
+        try {
+            return objectMapper.readValue(json,clase);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
